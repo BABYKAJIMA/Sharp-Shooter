397043c6f0bec854626c2d1a5108e4f4ca2bf11e
babykajima <kojimahaianh@gmail.com>
2025-03-28 23:17:20 +0700

    shooting and eme,y health

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animatior;
+    [SerializeField] GameObject hitVFXPrefab;   
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damgeAmount = 1;
+    
     StarterAssetsInputs starterAssetsInputs;
+
+    const string SHOOT_STRING = "Shoot";
 
     private void Awake()
     {
@@ -12,21 +18,26 @@ public class Weapon : MonoBehaviour
     }
     void Update()
     {
+        HandleShoot();
+    }
+
+   void HandleShoot()
+    {
         if (!starterAssetsInputs.shoot) return;
         {
+            muzzleFlash.Play();
+            animatior.Play(SHOOT_STRING, 0, 0f);
+            starterAssetsInputs.ShootInput(false);
+
             RaycastHit hit;
 
-            if( Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
             {
+                Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+
                 EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
                 enemyHealth?.TakeDamge(damgeAmount);
-                //if (enemyHealth)
-                //{
-                //    enemyHealth.TakeDamge(damgeAmount);
-                //}
-                    
-               starterAssetsInputs.ShootInput(false); 
             }
-        }     
+        }
     }
 }
